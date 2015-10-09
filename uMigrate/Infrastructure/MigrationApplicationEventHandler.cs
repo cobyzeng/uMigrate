a020aaea59190fcf84660a97c616961a38c62fb6
Andrey Shchekin <ashmind@gmail.com>
2015-10-09 23:00:21 +1300

    Added uMigrate:Enabled setting (closes gh-2).

--- uMigrate/Infrastructure/MigrationApplicationEventHandler.cs
+++ uMigrate/Infrastructure/MigrationApplicationEventHandler.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Security.Principal;
 using System.Web;
 using JetBrains.Annotations;
@@ -9,16 +11,43 @@ using uMigrate.Internal;
 namespace uMigrate.Infrastructure {
     [UsedImplicitly, PublicAPI]
     public class MigrationApplicationEventHandler : ApplicationEventHandler {
+        private const string EnabledAppSettingKey = "uMigrate:Enabled";
+
         // that's just for some performance optimizations, e.g. you may skip reacting to changes during migration.
         [PublicAPI] public static bool MigrationRunning { get; private set; }
 
         protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext) {
+            var logger = LogManager.GetLogger(typeof(MigrationApplicationEventHandler));
+
             MigrationRunning = true;
             try {
+                var enabled = GetEnabled();
+                if (!enabled) {
+                    logger.WarnFormat($"uMigrate is disabled (app setting {0} set to false), skipping.", EnabledAppSettingKey);
+                    return;
+                }
+
                 RunMigrations(applicationContext);
+            }
+            catch (Exception ex) {
+                logger.Fatal($"uMigrate failed.", ex);
+                throw;
             }
             finally {
                 MigrationRunning = false;
+            }
+        }
+
+        private static bool GetEnabled() {
+            var enabledString = ConfigurationManager.AppSettings[EnabledAppSettingKey];
+            if (string.IsNullOrEmpty(enabledString))
+                return true;
+
+            try {
+                return bool.Parse(enabledString);
+            }
+            catch (FormatException ex) {
+                throw new Exception($"Failed to parse app setting {EnabledAppSettingKey} as boolean: {ex.Message}.");
             }
         }
 
