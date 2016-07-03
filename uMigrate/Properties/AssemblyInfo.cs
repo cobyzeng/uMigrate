e33eb055130d18e5645ba5fc916696f701b4366f
Andrey Shchekin <ashmind@gmail.com>
2016-07-04 10:36:34 +1200

    SYMPHONY-3450 Version finalized.

--- uMigrate/Properties/AssemblyInfo.cs
+++ uMigrate/Properties/AssemblyInfo.cs
@@ -18,6 +18,6 @@ namespace uMigrate.Properties {
     public static class AssemblyInfo {
         // please follow SemVer here:
         public const string VersionString = "0.15.1";
-        public const string InformationalVersionString = VersionString + "-pre-01";
+        public const string InformationalVersionString = VersionString;
     }
 }
